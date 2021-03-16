0679ec4c654f01ab389c14ea5fc8ee87367e0339
Yuchuan(Vic) <80407135+ViccLe-e509@users.noreply.github.com>
2021-03-16 20:22:14 +0800

    Update "addRoom" method

--- RoomDataReaderInterface.java
+++ RoomDataReaderInterface.java
@@ -1,11 +1,13 @@
 import java.util.List;
 import java.util.zip.DataFormatException;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 
 public interface RoomDataReaderInterface {
 	
 	public List<Room> readDataSet(Reader inputFileReader) throws FileNotFoundException, IOException, DataFormatException;
-
+	
+	public boolean addRoom(FileWriter roomWriter, String[] newRoom);
 }
